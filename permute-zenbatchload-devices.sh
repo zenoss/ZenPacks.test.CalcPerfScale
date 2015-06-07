c7307615dcc6fcf9ce68b9286e3c2e2663aa04b7
Phil Lu <plu@zenoss.com>
2015-06-07 07:54:45 -0500

    add more aggregators to increase ratio agg/cp

--- permute-zenbatchload-devices.sh
+++ permute-zenbatchload-devices.sh
@@ -2,13 +2,11 @@
 
 ZB="/tmp/zenbatchload.txt"
 echo /Test/CalcPerf >$ZB
-for i in {1..15}; do
+for i in {11..25}; do
         IP="10.171.100.$i"  # these hosts are pingable simulated IPs from QA
-        echo "cphost$i setManageIp='$IP', setPerformanceMonitor='col$i', zTestCalcPerfTopComponentsPerDevice=5, zTestCalcPerfBottomComponentsPerTopComponent=2" >>$ZB
+        echo "cphost$i setManageIp='$IP', setPerformanceMonitor='col$i', zTestCalcPerfTopComponentsPerDevice=100, zTestCalcPerfBottomComponentsPerTopComponent=2" >>$ZB
 
         echo started host$i $IP
-
-        i=$(($i+1));
 done
 echo created $ZB
 cat $ZB
